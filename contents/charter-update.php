8d29bd66fd7f813ec691f697165b3631e2bbbd8d
Moinul <moinul2k7@gmail.com>
2024-01-02 13:01:19 +0600

    Update-Page Creation

--- contents/charter-update.php
+++ contents/charter-update.php
@@ -1,1 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta lang="en">
+    <link rel="icon" href="/images/govlogo.png" type="image/x-icon">
+    <link rel="stylesheet" href="/resources/css/main.css">
+    <link rel="stylesheet" href="/resources/css/fonts.css">
+    <title>সিটিজেন চার্টার হালনাগাদকরণ - স্মার্ট সিটিজেন চার্টার - অভ্যন্তরীণ সম্পদ বিভাগ, অর্থ মন্ত্রণালয়</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <header class="logged-inside">
+            <div class="office-logo">
+                <img src="/images/govlogo.png" alt="Office Logo">
+            </div>
+            <div class="office-name">
+                <p>গণপ্রজাতন্ত্রী বাংলাদেশ সরকার<br>
+                    অভ্যন্তরীণ সম্পদ বিভাগ<br>
+                    অর্থ মন্ত্রণালয়</p>
+            </div>
+        </header>
+
+        <main>
+
+            <div class="home-text">
+                <h2>সিটিজেন চার্টার হালনাগাদকরণ</h2>
+            </div>
+
+            <div class="home-action">
+                
+            </div>
+
+        </main>
+
+        <footer>
+            <p>কারিগরি সহায়তায়: আইসিটি সেল, অভ্যন্তরীণ সম্পদ বিভাগ</p>
+        </footer>
+    </div>
+
+</body>
+
+</html>
